da6f3cb158d60814b9d2344e56d58c7fc032a407
weitong <weitong@wangtiansoft.com>
2019-01-02 14:39:03 +0800

    修改持续集成环境文件地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   BASE_API: '"http://bwcar.wocloud1.wangtiansoft.com"',
-  FILE_SERVER_URL: '"http://bwcar.wocloud1.wangtiansoft.com/files"'
+  FILE_SERVER_URL: '"http://wocloud1.wangtiansoft.com/files"'
   // BASE_API: '"http://101.96.131.114:50010/server"'
 }
