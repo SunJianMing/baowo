a66d79a181c411d426ad381a7bbbba70b5d56bcd
weitong <weitong@wangtiansoft.com>
2019-01-02 14:10:03 +0800

    Update prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://103.241.165.244:50000/server"',
-  FILE_SERVER_URL: '"http://127.0.0.1:80/files"'
+  BASE_API: '"http://bwcar.wocloud1.wangtiansoft.com"',
+  FILE_SERVER_URL: '"http://bwcar.wocloud1.wangtiansoft.com/files"'
   // BASE_API: '"http://101.96.131.114:50010/server"'
 }
